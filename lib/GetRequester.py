dbbcf9472e9eb375c5f9e0d9f13a1f56eae376cd
victor kamau <vikakamau04@gmail.com>
2024-07-03 21:37:01 +0300

    final

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,18 @@
 import requests
 import json
 
+
+
 class GetRequester:
+
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
